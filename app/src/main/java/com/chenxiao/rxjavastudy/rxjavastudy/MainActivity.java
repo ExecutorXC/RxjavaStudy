a71c5c2a5dbec86ea7ac4f76b879475f5cf6b3e6
ExecutorXC <foxdown@163.com>
2018-01-02 11:45:07 +0800

    checkout test master

--- app/src/main/java/com/chenxiao/rxjavastudy/rxjavastudy/MainActivity.java
+++ app/src/main/java/com/chenxiao/rxjavastudy/rxjavastudy/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v("chenxiao", "I think it is useful");
-        Log.v("chenxiao", "This will be updated by ssl");
+        Log.v("chenxiao", "This is brach master");
+
     }
 }
