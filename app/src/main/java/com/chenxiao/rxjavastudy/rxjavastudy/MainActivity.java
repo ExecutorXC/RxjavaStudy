77ee51f1d58206c71e22700179d7e5c4483f7199
ExecutorXC <foxdown@163.com>
2018-01-02 11:29:31 +0800

    first ssl update

--- app/src/main/java/com/chenxiao/rxjavastudy/rxjavastudy/MainActivity.java
+++ app/src/main/java/com/chenxiao/rxjavastudy/rxjavastudy/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v("chenxiao", "I think it is useful");
+        Log.v("chenxiao", "This will be updated by ssl");
     }
 }
